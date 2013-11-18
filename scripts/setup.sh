0cdab2e15051458d9ef0510f91e7ed2cc03c48f1
Darron Froese <darron@froese.org>
2013-11-18 10:20:20 -0700

    Adding Ansible and Chef provisioners.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # Update all packages.
-sudo apt-get update
 sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:rquillo/ansible
+sudo apt-get update
 sudo apt-get -y upgrade
 
 /usr/sbin/update-locale
 
 # install the backported kernel
 sudo apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
+
+# Install Chef
+curl -L https://www.opscode.com/chef/install.sh | sudo bash
+
+# Install Ansible
+sudo apt-get install -y ansible
