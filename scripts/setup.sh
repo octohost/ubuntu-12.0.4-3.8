1eee8f90d592f200c900e5c82491a53803456d65
Darron Froese <darron@froese.org>
2014-04-03 12:09:32 -0600

    Update the locale.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,14 @@ sudo add-apt-repository -y ppa:rquillo/ansible
 sudo apt-get update
 sudo apt-get -y upgrade
 
-/usr/sbin/update-locale
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+sudo locale-gen en_US.UTF-8
+sudo dpkg-reconfigure locales
+sudo touch /root/.locale-fixed
+sudo /usr/sbin/update-locale
 
 # install the backported kernel
 sudo apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
